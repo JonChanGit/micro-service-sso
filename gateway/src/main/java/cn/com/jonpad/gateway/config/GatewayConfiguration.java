59db19f9b7c2522bacf4dc0c1a3583ce7188a255
JonChan <chenjunlai@ffcs.cn>
2019-01-17 14:56:58 +0800

    Spring Cloud Gateway基本用法，OAuth无效

---
+++ gateway/src/main/java/cn/com/jonpad/gateway/config/GatewayConfiguration.java
@@ -0,0 +1,11 @@
+package cn.com.jonpad.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Jon Chan
+ * @date 2019/1/17 14:50
+ */
+@Configuration
+public class GatewayConfiguration {
+}
